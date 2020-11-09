ae08a1c6761c972105dddb406dbeb4a535a83f64
Roberto-Livi <robertomlivi@outlook.com>
2020-11-09 11:22:07 -0500

    created a StaticTest class with a private static property, cosntructor, and getters

--- code/src/com/livi/Main.java
+++ code/src/com/livi/Main.java
@@ -3,7 +3,17 @@ package com.livi;
 public class Main {
 
     public static void main(String[] args) {
+        StaticTest firstInstance = new StaticTest("1st Instance");
+        System.out.println(firstInstance.getName() + " is instance number " + StaticTest.getNumInstances());
 
+        StaticTest secondInstance = new StaticTest("2nd Instance");
+        System.out.println(secondInstance.getName() + " is instance number " + StaticTest.getNumInstances());
+
+        StaticTest thirdInstance = new StaticTest("3rd Instance");
+        System.out.println(thirdInstance.getName() + " is instance number " + StaticTest.getNumInstances());
+
+        // Running a Java program from the command line example
+        // java com.livi._static.Main
     }
 
 }
---
+++ code/src/com/livi/StaticTest.java
@@ -0,0 +1,19 @@
+package com.livi;
+
+public class StaticTest {
+    private static int numOfInstances = 0;
+    private String name;
+
+    public StaticTest(String name) {
+        this.name = name;
+        numOfInstances++;
+    }
+
+    public static int getNumInstances() {
+        return numOfInstances;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
